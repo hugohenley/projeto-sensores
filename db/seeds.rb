151cc7162c428c608f21d1e646871183ed535ca4
Hugo Henley <hugohenley@gmail.com>
2013-12-19 16:36:39 -0200

    adding seeds to make it easier to create random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def random_latitude
+  -22.67 + rand(0.001..0.0099)
+end
+
+def random_longitude
+  -43.38 + rand(0.001..0.0099)
+end
+
+200.times do
+  PositionDetail.create( { phone_model: 'iPhone 5', carrier: 'VIVO', signal: '-23', latitude: random_latitude, longitude: random_longitude, timestamp: 1929310923 } )
+end
